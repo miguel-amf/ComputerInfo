054e228106b0ad9559531140cce03f2e57123597
Live session user <dev@miguelmontagner.com>
2023-06-23 17:05:27 +0000

    Fixed for some HP Batteries

--- parsers/BatteryData.py
+++ parsers/BatteryData.py
@@ -3,26 +3,46 @@ from os import path
 from math import floor
 
 def _get_battery_max_percentage():
-    chargeDesign = 100
-    chargeCurrent = 0
+    chargeDesign = 1000
+    chargeCurrent = -1
     cycles = 0
-    if path.exists('/sys/class/power_supply/BAT0/charge_full_design') :
-        with open('/sys/class/power_supply/BAT0/charge_full_design') as f:
-            chargeDesign = int(f.read().strip())
-    if path.exists('/sys/class/power_supply/BAT0/charge_full_design') :
-        with open('/sys/class/power_supply/BAT0/charge_full') as f:
-            chargeCurrent = int(f.read().strip())
-    if path.exists('/sys/class/power_supply/BAT0/cycle_count') :
-        with open('/sys/class/power_supply/BAT0/cycle_count') as f:
-            cycles = int(f.read().strip())
-    
-    return {'life' : (chargeCurrent/chargeDesign)*100,  'cycles': cycles}
+
+    batteries = []
+
+    #path normally follows the format of /sys/class/power_supply/BAT*/, se we got to check for every BAT* possible
+    initialPathStr = '/sys/class/power_supply/BAT'
+
+    batIterator = 0
+
+    while(True) :
+        if path.exists('/sys/class/power_supply/BAT' + str(batIterator) + '/'):
+            #check for design and different naming types
+            if path.exists('/sys/class/power_supply/BAT' + str(batIterator) + '/charge_full_design') :
+                with open('/sys/class/power_supply/BAT' + str(batIterator) + '/charge_full_design') as f:
+                    chargeDesign = int(f.read().strip())
+            elif path.exists('/sys/class/power_supply/BAT0/energy_full_design'):
+                with open('/sys/class/power_supply/BAT0/energy_full_design') as f:
+                    chargeDesign = int(f.read().strip())
+
+            if path.exists('/sys/class/power_supply/BAT' + str(batIterator) + '/charge_full') :
+                with open('/sys/class/power_supply/BAT' + str(batIterator) + '/charge_full') as f:
+                    chargeCurrent = int(f.read().strip())
+
+            elif path.exists('/sys/class/power_supply/BAT' + str(batIterator) + '/energy_full'):
+                with open('/sys/class/power_supply/BAT' + str(batIterator) + '/energy_full') as f:
+                    chargeCurrent = int(f.read().strip())
+            batteries.append([batIterator,(chargeCurrent/chargeDesign)*100])
+            batIterator += 1
+        else:
+            break
+    return batteries
 
 
 
 def getBatteryData():
-	if (path.exists("/sys/class/power_supply/BAT0/")) :
-		bat_result = _get_battery_max_percentage()
-		return "Battery:\t\t" + str(floor(bat_result['life'])) + "%|Cycles: " + str(bat_result['cycles']) + '\n'
-	else:
-		return "Battery:\t\tNo Battery Detected"
+    returnStr = ''
+    batCount = 1
+    for bat in _get_battery_max_percentage():
+        returnStr += "Battery " + str(batCount) + ":\t" + str(floor(bat[1])) + "%\n"
+        batCount += 1
+    return returnStr
